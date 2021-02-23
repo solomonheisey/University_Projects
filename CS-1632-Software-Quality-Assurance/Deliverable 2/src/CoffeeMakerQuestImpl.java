4a929d4c28d0253d68f9557f0304e669156ca474
Solomon Heisey <49796963+solomonheisey@users.noreply.github.com>
2021-02-23 16:53:11 -0500

    initial files

---
+++ CS-1632-Software-Quality-Assurance/Deliverable 2/src/CoffeeMakerQuestImpl.java
@@ -0,0 +1,115 @@
+import java.util.*;
+
+public class CoffeeMakerQuestImpl implements CoffeeMakerQuest {
+
+	// TODO: Add more member variables and methods as needed.
+	
+	CoffeeMakerQuestImpl() {
+		// TODO
+	}
+
+	/**
+	 * Whether the game is over. The game ends when the player drinks the coffee.
+	 * 
+	 * @return true if successful, false otherwise
+	 */
+	public boolean isGameOver() {
+		// TODO
+		return false;
+	}
+
+	/**
+	 * Set the player to p.
+	 * 
+	 * @param p the player
+	 */
+	public void setPlayer(Player p) {
+		// TODO
+	}
+	
+	/**
+	 * Add the first room in the game. If room is null or if this not the first room
+	 * (there are pre-exiting rooms), the room is not added and false is returned.
+	 * 
+	 * @param room the room to add
+	 * @return true if successful, false otherwise
+	 */
+	public boolean addFirstRoom(Room room) {
+		// TODO
+		return false;
+	}
+
+	/**
+	 * Attach room to the northern-most room. If either room, northDoor, or
+	 * southDoor are null, the room is not added. If there are no pre-exiting rooms,
+	 * the room is not added. If room is not a unique room (a pre-exiting room has
+	 * the same adjective or furnishing), the room is not added. If all these tests
+	 * pass, the room is added. Also, the north door of the northern-most room is
+	 * labeled northDoor and the south door of the added room is labeled southDoor.
+	 * Of course, the north door of the new room is still null because there is
+	 * no room to the north of the new room.
+	 * 
+	 * @param room      the room to add
+	 * @param northDoor string to label the north door of the current northern-most room
+	 * @param southDoor string to label the south door of the newly added room
+	 * @return true if successful, false otherwise
+	 */
+	public boolean addRoomAtNorth(Room room, String northDoor, String southDoor) {
+		// TODO
+		return false;
+	}
+
+	/**
+	 * Returns the room the player is currently in. If location of player has not
+	 * yet been initialized with setCurrentRoom, returns null.
+	 * 
+	 * @return room player is in, or null if not yet initialized
+	 */ 
+	public Room getCurrentRoom() {
+		// TODO
+		return null;
+	}
+	
+	/**
+	 * Set the current location of the player. If room does not exist in the game,
+	 * then the location of the player does not change and false is returned.
+	 * 
+	 * @param room the room to set as the player location
+	 * @return true if successful, false otherwise
+	 */
+	public boolean setCurrentRoom(Room room) {
+		// TODO
+		return false;
+	}
+	
+	/**
+	 * Get the instructions string command prompt. It returns the following prompt:
+	 * " INSTRUCTIONS (N,S,L,I,D,H) > ".
+	 * 
+	 * @return comamnd prompt string
+	 */
+	public String getInstructionsString() {
+		// TODO
+		return "";
+	}
+	
+	/**
+	 * Processes the user command given in String cmd and returns the response
+	 * string. For the list of commands, please see the Coffee Maker Quest
+	 * requirements documentation (note that commands can be both upper-case and
+	 * lower-case). For the response strings, observe the response strings printed
+	 * by coffeemaker.jar. The "N" and "S" commands potentially change the location
+	 * of the player. The "L" command potentially adds an item to the player
+	 * inventory. The "D" command drinks the coffee and ends the game. Make
+     * sure you use Player.getInventoryString() whenever you need to display
+     * the inventory.
+	 * 
+	 * @param cmd the user command
+	 * @return response string for the command
+	 */
+	public String processCommand(String cmd) {
+		// TODO
+		return "";
+	}
+	
+}
---
+++ CS-1632-Software-Quality-Assurance/Deliverable 2/src/CoffeeMakerQuestTest.java
@@ -0,0 +1,199 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.*;
+import org.mockito.*;
+import static org.mockito.Mockito.*;
+
+public class CoffeeMakerQuestTest {
+
+	CoffeeMakerQuest cmq;
+	Player player;
+	Room room1;	// Small room
+	Room room2;	// Funny room
+	Room room3;	// Refinanced room
+	Room room4;	// Dumb room
+	Room room5;	// Bloodthirsty room
+	Room room6;	// Rough room
+
+	@Before
+	public void setup() {
+		// 0. Turn on bug injection for Player and Room.
+		Config.setBuggyPlayer(true);
+		Config.setBuggyRoom(true);
+		
+		// 1. Create the Coffee Maker Quest object and assign to cmq.
+		cmq = CoffeeMakerQuest.createInstance();
+
+		// TODO: 2. Create a mock Player and assign to player and call cmq.setPlayer(player). 
+		// Player should not have any items (no coffee, no cream, no sugar)
+
+		// TODO: 3. Create mock Rooms and assign to room1, room2, ..., room6.
+		// Mimic the furnishings / adjectives / items of the rooms in the original Coffee Maker Quest.
+		
+		// TODO: 4. Add the rooms created above to mimic the layout of the original Coffee Maker Quest.
+		
+	}
+
+	@After
+	public void tearDown() {
+	}
+	
+	/**
+	 * Test case for String getInstructionsString().
+	 * Preconditions: None.
+	 * Execution steps: Call cmq.getInstructionsString().
+	 * Postconditions: Return value is " INSTRUCTIONS (N,S,L,I,D,H) > ".
+	 */
+	@Test
+	public void testGetInstructionsString() {
+		// TODO
+	}
+	
+	/**
+	 * Test case for boolean addFirstRoom(Room room).
+	 * Preconditions: room1 ~ room6 have been added to cmq.
+	 *                Create a mock room and assign to myRoom.
+	 * Execution steps: Call cmq.addFirstRoom(myRoom).
+	 * Postconditions: Return value is false.
+	 */
+	@Test
+	public void testAddFirstRoom() {
+		// TODO
+	}
+	
+	/**
+	 * Test case for boolean addRoomAtNorth(Room room, String northDoor, String southDoor).
+	 * Preconditions: room1 ~ room6 have been added to cmq.
+	 *                Create a mock "Fake" room with "Fake bed" furnishing with no item, and assign to myRoom.
+	 * Execution steps: Call cmq.addRoomAtNorth(myRoom, "North", "South").
+	 * Postconditions: Return value is true.
+	 *                 room6.setNorthDoor("North") is called.
+	 *                 myRoom.setSouthDoor("South") is called.
+	 */
+	@Test
+	public void testAddRoomAtNorthUnique() {
+		// TODO
+	}
+	
+	/**
+	 * Test case for boolean addRoomAtNorth(Room room, String northDoor, String southDoor).
+	 * Preconditions: room1 ~ room6 have been added to cmq.
+	 *                Create a mock "Fake" room with "Flat energy drink" furnishing with no item, and assign to myRoom.
+	 * Execution steps: Call cmq.addRoomAtNorth(myRoom, "North", "South").
+	 * Postconditions: Return value is false.
+	 *                 room6.setNorthDoor("North") is not called.
+	 *                 myRoom.setSouthDoor("South") is not called.
+	 */
+	@Test
+	public void testAddRoomAtNorthDuplicate() {
+		// TODO
+	}
+	
+	/**
+	 * Test case for Room getCurrentRoom().
+	 * Preconditions: room1 ~ room6 have been added to cmq.
+	 *                cmq.setCurrentRoom(Room) has not yet been called.
+	 * Execution steps: Call cmq.getCurrentRoom().
+	 * Postconditions: Return value is null.
+	 */
+	@Test
+	public void testGetCurrentRoom() {
+		// TODO
+	}
+	
+	/**
+	 * Test case for void setCurrentRoom(Room room) and Room getCurrentRoom().
+	 * Preconditions: room1 ~ room6 have been added to cmq.
+	 *                cmq.setCurrentRoom(Room room) has not yet been called.
+	 * Execution steps: Call cmq.setCurrentRoom(room3).
+	 *                  Call cmq.getCurrentRoom().
+	 * Postconditions: Return value of cmq.setCurrentRoom(room3) is true. 
+	 *                 Return value of cmq.getCurrentRoom() is room3.
+	 */
+	@Test
+	public void testSetCurrentRoom() {
+		// TODO
+	}
+	
+	/**
+	 * Test case for String processCommand("I").
+	 * Preconditions: Player does not have any items.
+	 * Execution steps: Call cmq.processCommand("I").
+	 * Postconditions: Return value is "YOU HAVE NO COFFEE!\nYOU HAVE NO CREAM!\nYOU HAVE NO SUGAR!\n".
+	 */
+	@Test
+	public void testProcessCommandI() {
+		// TODO
+	}
+	
+	/**
+	 * Test case for String processCommand("l").
+	 * Preconditions: room1 ~ room6 have been added to cmq.
+	 *                cmq.setCurrentRoom(room1) has been called.
+	 * Execution steps: Call cmq.processCommand("l").
+	 * Postconditions: Return value is "There might be something here...\nYou found some creamy cream!\n".
+	 *                 player.addItem(Item.CREAM) is called.
+	 */
+	@Test
+	public void testProcessCommandLCream() {
+		// TODO
+	}
+	
+	/**
+	 * Test case for String processCommand("n").
+	 * Preconditions: room1 ~ room6 have been added to cmq.
+	 *                cmq.setCurrentRoom(room4) has been called.
+	 * Execution steps: Call cmq.processCommand("n").
+	 *                  Call cmq.getCurrentRoom().
+	 * Postconditions: Return value of cmq.processCommand("n") is "".
+	 *                 Return value of cmq.getCurrentRoom() is room5.
+	 */
+	@Test
+	public void testProcessCommandN() {
+		// TODO
+	}
+	
+	/**
+	 * Test case for String processCommand("s").
+	 * Preconditions: room1 ~ room6 have been added to cmq.
+	 *                cmq.setCurrentRoom(room1) has been called.
+	 * Execution steps: Call cmq.processCommand("s").
+	 *                  Call cmq.getCurrentRoom().
+	 * Postconditions: Return value of cmq.processCommand("s") is "A door in that direction does not exist.\n".
+	 *                 Return value of cmq.getCurrentRoom() is room1.
+	 */
+	@Test
+	public void testProcessCommandS() {
+		// TODO
+	}
+	
+	/**
+	 * Test case for String processCommand("D").
+	 * Preconditions: Player has no items.
+	 * Execution steps: Call cmq.processCommand("D").
+	 *                  Call cmq.isGameOver().
+	 * Postconditions: Return value of cmq.processCommand("D") is "YOU HAVE NO COFFEE!\nYOU HAVE NO CREAM!\nYOU HAVE NO SUGAR!\n\nYou drink the air, as you have no coffee, sugar, or cream.\nThe air is invigorating, but not invigorating enough. You cannot study.\nYou lose!\n".
+	 *                 Return value of cmq.isGameOver() is true.
+	 */
+	@Test
+	public void testProcessCommandDLose() {
+		// TODO
+	}
+	
+	/**
+	 * Test case for String processCommand("D").
+	 * Preconditions: Player has all 3 items (coffee, cream, sugar).
+	 * Execution steps: Call cmq.processCommand("D").
+	 *                  Call cmq.isGameOver().
+	 * Postconditions: Return value of cmq.processCommand("D") is "You have a cup of delicious coffee.\nYou have some fresh cream.\nYou have some tasty sugar.\n\nYou drink the beverage and are ready to study!\nYou win!\n".
+	 *                 Return value of cmq.isGameOver() is true.
+	 */
+	@Test
+	public void testProcessCommandDWin() {
+		// TODO
+	}
+	
+	// TODO: Put in more unit tests of your own making to improve coverage!
+	
+}
